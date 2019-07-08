d1fd1bcde8f8253d1e019d3db1bff9ea51a53a4f
nyko07 <nyko07herr@gmail.com>
2019-07-08 01:14:59 -0500

    Avance
    Con conexiones entre todas las vistas y organización del proyecto según estándar Django

--- ProyectoModelosII/gaming/models.py
+++ ProyectoModelosII/gaming/models.py
@@ -1,35 +1,42 @@
 from django.db import models
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-class Plataforma(models.Model):
-    listadoJuegos = models.CharField(max_length=200)
+class Perfil(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=255, blank=True)
+    web = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.usuario.username
+
+
+@receiver(post_save, sender=User)
+def crear_usuario_perfil(sender, instance, created, **kwargs):
+    if created:
+        Perfil.objects.create(usuario=instance)
+
+
+@receiver(post_save, sender=User)
+def guardar_usuario_perfil(sender, instance, **kwargs):
+    instance.perfil.save()
 
 
 class Tipo(models.Model):
     nombre = models.CharField(max_length=50)
-    tipo = models.CharField(max_length=50)
+
+
+class Puntaje(models.Model):
+    valor = models.IntegerField(default=0)
+    fecha = models.DateTimeField('fecha puntaje')
 
 
 class Recomendado(models.Model):
     tipo = models.ForeignKey(Tipo, on_delete=models.CASCADE)
     cantidad = models.IntegerField(default=0)
-
-
-class Jugador(models.Model):
-    plataforma = models.ForeignKey(Plataforma, on_delete=models.CASCADE)
-    tiposRecomendados = models.ForeignKey(Recomendado, on_delete=models.CASCADE)
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    alias = models.CharField(max_length=50)
-    contrasena = models.CharField('password', max_length=50)
-    fechaNacimiento = models.DateTimeField('fecha nacimiento')
-
-
-class Puntaje(models.Model):
-    jugador = models.ForeignKey(Jugador, on_delete=models.CASCADE)
-    valor = models.IntegerField(default=0)
-    fecha = models.DateTimeField('fecha puntaje')
 
 
 class Juego(models.Model):
@@ -39,9 +46,21 @@ class Juego(models.Model):
     creador = models.CharField(max_length=200)
     fechaCreacion = models.DateTimeField('fecha Creacion')
 
+    def listarJuegos(self):
+        pass
 
-class TipoAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'tipo')
+
+class Jugador(models.Model):
+    juego = models.ManyToManyField(Juego)
+    tiposRecomendados = models.ManyToManyField(Recomendado)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    alias = models.CharField(max_length=50)
+    contrasena = models.CharField('password', max_length=50)
+    fechaNacimiento = models.DateTimeField('fecha nacimiento')
+
+    def listarJugadores(self):
+        pass
 
 
 class RecomendadoAdmin(admin.ModelAdmin):
@@ -53,7 +72,7 @@ class JugadorAdmin(admin.ModelAdmin):
 
 
 class PuntajeAdmin(admin.ModelAdmin):
-    list_display = ('jugador', 'valor', 'fecha')
+    list_display = ('valor', 'fecha')
 
 
 class JuegoAdmin(admin.ModelAdmin):
